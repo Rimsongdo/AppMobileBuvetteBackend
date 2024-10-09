9529391f15359a704f965cf138ffad1562edebd3
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-10-09 20:54:38 +0100

    first

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config
 const PORT=5000
-const MONGODB_URL="mongodb://0.0.0.0:27017/"
-//const MONGODB_URL="mongodb+srv://user2000:test234@cluster0.i1qvjcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//const MONGODB_URL="mongodb://0.0.0.0:27017/"
+const MONGODB_URL="mongodb+srv://Roger:0Rimsongdo0@reacttest.ntnjvtx.mongodb.net/?retryWrites=true&w=majority&appName=reactTest"
 const SECRET='ROGER'
 
 module.exports={PORT,MONGODB_URL,SECRET}
